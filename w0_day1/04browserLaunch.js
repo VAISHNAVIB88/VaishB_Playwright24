ce0b52c0281661368566828c2a80381583fcb9a0
VAISHNAVI BABU <92648452+VAISHNAVIB88@users.noreply.github.com>
2024-09-23 01:00:01 +1200

    Update 04browserLaunch.js

--- w0_day1/04browserLaunch.js
+++ w0_day1/04browserLaunch.js
@@ -1,31 +1,16 @@
-// var gender ='female';
-// function printGender(){
-//     if (gender. startsWith('female'))
-//         {
-//             console.log("she/her");
-//             else
-//             console.log("he/him");
-        
-//         }
-// }
-
 const browserVersion ='chrome';
-
 function getBrowserVersion (){
-    if (browserVersion.startsWith('edge')){
-        let browserVersion= "121.3021.31255";
-        console.log("Browser version is"+ " "+ browserVersion);
-
-            }
-            else if (browserVersion.startsWith('chrome')){
-                let browserVersion = "You are Using Chrome browser";
-                console.log(" Browser Version is "+ " "+ browserVersion);
-                }
-                else{
-                    let browserVersion=null;
-                    console.log("Browser version is"+" "+ browserVersion);
-                    
-                }
-
+if (browserVersion.startsWith('chrome')){
+let browserVersion= "128.0.6613.138";
+console.log("Browser version is"+ " "+ browserVersion);
+}
+else if (browserVersion.startsWith('edge')){
+let browserVersion = "You are in Edge browser";
+console.log(" Browser Version is "+ " "+ browserVersion);
+}
+else{
+let browserVersion=null;
+console.log("Browser version is"+" "+ browserVersion);
+}
 }
 getBrowserVersion();
