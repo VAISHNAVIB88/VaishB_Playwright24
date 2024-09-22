bbc243d9f89fb529255dcbad4e5e16bee7b6e49d
VAISHNAVI BABU <92648452+VAISHNAVIB88@users.noreply.github.com>
2024-09-23 00:56:28 +1200

    Update 02datatype.js

--- w0_day1/02datatype.js
+++ w0_day1/02datatype.js
@@ -1,19 +1,13 @@
 var username = "Testleaf";
 var firstname = 'vaishnavi';
 var character ="R";
-
 console.log(typeof character);
-
-
 var age =28;
 console.log( typeof age);
 
-
 //Boolean
-
 var isRaining = true;
 console.log(typeof isRaining);
-
 
 //undefined
 var registrationnumber;
