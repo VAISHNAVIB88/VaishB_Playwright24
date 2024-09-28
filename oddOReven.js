c45113178c01c63de093c2265841c33f02bc2651
VAISHNAVI BABU <92648452+VAISHNAVIB88@users.noreply.github.com>
2024-09-28 20:01:45 +1200

    Add files via upload

---
+++ oddOReven.js
@@ -0,0 +1,9 @@
+function isOddOrEven(myNumber) 
+   {
+        if (myNumber % 2 == 0)
+        console.log("Provided Number is Even");
+        else
+        console.log("Provided number is Odd");
+   }
+let number = 298;
+isOddOrEven(number);
